f69e42b4b9ebfc36fc724fb69da3cd715fbc24a1
Avisek <aviseknaug1993@gmail.com>
2020-06-24 11:50:37 -0500

    added makedir

--- source/utils.py
+++ source/utils.py
@@ -1,3 +1,16 @@
 """
 Utilities to perform some common functions
 """
+import os
+import shutil
+
+def make_dirs(dir_path):
+	try:
+		os.makedirs(dir_path)
+	except FileExistsError:
+		files = os.listdir(dir_path)
+		for f in files:
+			try:
+				shutil.rmtree(dir_path + f)
+			except NotADirectoryError:
+				os.remove(dir_path + f)
