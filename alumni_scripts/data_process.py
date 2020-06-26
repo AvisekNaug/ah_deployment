7008af26056581b3596b4717df59468b13b8cfcb
Avisek <aviseknaug1993@gmail.com>
2020-06-26 13:52:01 -0500

    add online api and cleaning

--- alumni_scripts/data_process.py
+++ alumni_scripts/data_process.py
@@ -27,13 +27,13 @@ def pull_offline_data(*args, **kwargs):
 	This method pulls data from the Niagara Networks using the BdX API
 	"""
 
-	# tz_utc = timezone(offset=-timedelta(hours=0))
+	tz_utc = timezone(offset=-timedelta(hours=0))
 	tz_central = timezone(offset=-timedelta(hours=6))
 
 	start = datetime(kwargs['start_year'], kwargs['start_month'], kwargs['start_day'], hour= kwargs['start_hour'],
-	 minute= kwargs['start_minute'], second= kwargs['start_second'], tzinfo=tz_central)
+	 minute= kwargs['start_minute'], second= kwargs['start_second'], tzinfo=tz_utc)
 	end   = datetime(kwargs['end_year'], kwargs['end_month'], kwargs['end_day'], hour= kwargs['end_hour'],
-	 minute= kwargs['end_minute'], second= kwargs['end_second'], tzinfo=tz_central)
+	 minute= kwargs['end_minute'], second= kwargs['end_second'], tzinfo=tz_utc)
 
 	dataframe = get_trend(trend_id=kwargs['trend_id'],
 						  username=kwargs['username'],
@@ -49,8 +49,9 @@ def offline_batch_data_clean(*args, **kwargs):
 	"""
 	remove outliers from offline batch data
 	"""
-	with open(kwargs['meta_data_path'], 'r') as fp:
-		meta_data_ = json.load(fp)
+	# with open(kwargs['meta_data_path'], 'r') as fp:
+	# 	meta_data_ = json.load(fp)
+	meta_data_ = kwargs['meta_data_']
 	meta_data = meta_data_.copy()
 	for key, value in meta_data_['column_stats'].items():
 		if value['std'] == 0:
@@ -74,9 +75,54 @@ def offline_batch_data_clean(*args, **kwargs):
 
 	return df
 
+# get online date
+def pull_online_data(*args, **kwargs):
+	"""
+	This method pulls data from the Niagara Networks using the BdX API
+	"""
+
+	tz_utc = timezone(offset=-timedelta(hours=0))
+	tz_central = timezone(offset=-timedelta(hours=6))
+
+	start = datetime(kwargs['start_year'], kwargs['start_month'], kwargs['start_day'], hour= kwargs['start_hour'],
+	 minute= kwargs['start_minute'], second= kwargs['start_second'], tzinfo=tz_utc)
+	end   = datetime(kwargs['end_year'], kwargs['end_month'], kwargs['end_day'], hour= kwargs['end_hour'],
+	 minute= kwargs['end_minute'], second= kwargs['end_second'], tzinfo=tz_utc)
+
+	dataframe = get_trend(trend_id=kwargs['trend_id'],
+						  username=kwargs['username'],
+						  password=kwargs['password'],
+						  start=start,
+						  end=end)
+
+	dataframe.to_csv(kwargs['save_path'])
 
 # online data clean
 def online_data_clean(*args, **kwargs):
 
-	pass
+	# with open(kwargs['meta_data_path'], 'r') as fp:
+	# 	meta_data_ = json.load(fp)
+	meta_data_ = kwargs['meta_data_']
+	meta_data = meta_data_.copy()
+	for key, value in meta_data_['column_stats'].items():
+		if value['std'] == 0:
+			meta_data['column_stats'][key]['std'] = 0.0001  # add small std for constant values
+	stats = pd.DataFrame(meta_data['column_stats'])
+
+	# Perform 2 standard deviation based thresholding
+	df = kwargs['df'].copy()
+	faulty_idx = np.abs(df-stats.loc['mean',:]) >= (2*stats.loc['std',:])
+
+	retain = False
+	if retain:
+		for col_name in df.columns:
+			#df.loc[faulty_idx[col_name], col_name] = stats.loc['mean', col_name]  # set to mean
+			t = (np.sign(df - stats.loc['mean', :]).loc[faulty_idx[col_name], col_name] *
+			 2 *stats.loc['std', col_name]) + stats.loc['mean', col_name]
+			df.loc[faulty_idx[col_name], col_name] = t  # set to upper or lower 2*std bound
+											
+	else:
+		df = df[~faulty_idx]  # keep rows which are within bounds
+
+	return df
 
